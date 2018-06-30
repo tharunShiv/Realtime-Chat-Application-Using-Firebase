9813fbc381361437c801d9777235c642e47dd749
tharunShiv <tharun.rocky14@gmail.com>
2018-06-30 23:04:52 +0530

    Login working perfectly

--- app/src/main/java/com/tharunshiv/flashchatnewfirebase/LoginActivity.java
+++ app/src/main/java/com/tharunshiv/flashchatnewfirebase/LoginActivity.java
@@ -2,19 +2,29 @@ package com.tharunshiv.flashchatnewfirebase;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class LoginActivity extends AppCompatActivity {
 
     // TODO: Add member variables here:
     // UI references.
+    private FirebaseAuth mAuth;
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
 
@@ -38,12 +48,13 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         // TODO: Grab an instance of FirebaseAuth
-
+        mAuth  = FirebaseAuth.getInstance();
     }
 
     // Executed when Sign in button pressed
     public void signInExistingUser(View v)   {
         // TODO: Call attemptLogin() here
+        attemptLogin();
 
     }
 
@@ -59,13 +70,47 @@ public class LoginActivity extends AppCompatActivity {
 
 
         // TODO: Use FirebaseAuth to sign in with email & password
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
 
+        // if email or password are blank
+        if(email.equals("") || password.equals(""))
+          return;
+
+        Toast.makeText(this, "Login in progress...", Toast.LENGTH_SHORT).show();
+
+        // talking to firebase
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d("Firechat", "signInWithEmail() onComplete: " + task.isSuccessful());
+
+                if (!task.isSuccessful()) {
+                    Log.d("Firechat", "Problem signing in: " + task.getException());
+
+                    showErrorDialog("There was a problem Signing in...");
+                } else {
+                    // call the next Activity
+                    Intent newIntent = new Intent(LoginActivity.this, MainChatActivity.class);
+                    finish();
+                    startActivity(newIntent);
+                }
+            }
+        });
 
 
     }
 
     // TODO: Show error on screen with an alert dialog
+    private void showErrorDialog(String message) {
 
+        new AlertDialog.Builder(this)
+                .setTitle("Oops")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
 
 
 }
